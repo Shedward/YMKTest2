19659772217957fead4c6fc183b080f0a5d2fc97
Vlad Maltsev <maltsevvn@m2.ru>
2021-05-25 12:26:23 +0300

    Перенос пути на внешний адресс.

--- YandexMapsWrapper/Package.swift
+++ YandexMapsWrapper/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "YandexMapsMobile",
-			url: "https://nexus.cloud.vtblife.ru/repository/mobile-snapshot/mobile/yandexMapKit/yandexMapsMobile-4.0.0-full/YandexMapsMobile.xcframework.zip",
+			url: "https://github.com/Shedward/YMK.xcframework/releases/download/4.0.0-full/YandexMapsMobile.xcframework-4.0.0-full.zip",
 			checksum: "01b9ec6b93eac28dbb38edf1ad751294bcb13a27b5f625b05d77791e8ecd24a0"
 		),
 		.target(
